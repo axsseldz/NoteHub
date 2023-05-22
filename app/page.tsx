975c1d27e4d182323a2be22f70b409dd4aa8126a
axel <axssel.diazfl@gmail.com>
2023-05-21 19:40:59 -0700

    Home page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-
+import { DiUnitySmall } from 'react-icons/di'
 
 export default function Home() {
   return (
-    <main>
-      Home
+    <main className="flex-1 flex flex-col relative">
+      <DiUnitySmall className='absolute left-[225px] text-gray-300 text-[700px]' />
+      <h1 className='absolute top-[650px] left-[620px] text-gray-300 text-3xl'>NoteHub®</h1>
     </main>
   )
 }
